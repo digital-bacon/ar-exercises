cb6c478153b67a3ffa161340a5e5612d63a2f533
Mark Bieganek <mark.bieganek@telus.com>
2023-01-23 22:03:33 -0700

    Add solution exercise_1.rb

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -4,3 +4,30 @@ puts "Exercise 1"
 puts "----------"
 
 # Your code goes below here ...
+
+stores = [
+  {
+    name: "Burnaby",
+    annual_revenue: 300000,
+    mens_apparel: true,
+    womens_apparel: true
+  },
+  {
+    name: "Richmond",
+    annual_revenue: 1260000,
+    mens_apparel: true,
+    womens_apparel: true
+  },
+  {
+    name: "Gastown",
+    annual_revenue: 190000,
+    mens_apparel: true,
+    womens_apparel: true
+  }
+]
+
+stores.each do |store|
+  Store.create(store)
+end
+
+puts Store.count
